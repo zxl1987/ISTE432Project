5ef50d66758591530c20a850a61dfa9f2cf60329
fxl2968 <55754876+Fenglin2968@users.noreply.github.com>
2019-10-25 16:48:00 -0400

    Exception

--- Project/Model/WeatherData.py
+++ Project/Model/WeatherData.py
@@ -4,28 +4,29 @@ from adapterData import *
 class Getdata:
     datainfo = None
     weatherinfo=None
-    def __init__(self, data):
-        self.datainfo = data
+
+    def __init__(self, option, city):
+        getWeather = apiData(option, city)
+        adapter = Adapter(getWeather)
+        self.datainfo = adapter
+        self.processData()
+
 
     def processData(self):
         data=self.datainfo.alldata()
-        minTemp=(int(data['main']['temp_min'])-273.15) * 9/5+32
-        maxTemp=(int(data['main']['temp_max'])-273.15) * 9/5+32
-        Temp = (int(data['main']['temp'])-273.15) * 9/5+32
-        self.weatherinfo=[round(minTemp, 2), round(maxTemp, 2), round(Temp, 2), data['wind']['speed'], data['weather'][0]['description']]
-        self.returnWeather()
+        if 'message' in data:
+            if 'Invalid API key' in data['message']:
+                self.weatherinfo= "Can't connect to the API or server is down. Try again later"
+            if 'city not found' in data['message']:
+                self.weatherinfo = "City Not found!. Please type correct city"
+            if '400' in data['cod']:
+                self.weatherinfo="Can't find this geographic coordinates"
+        else:
+            minTemp = (int(data['main']['temp_min']) - 273.15) * 9 / 5 + 32
+            maxTemp = (int(data['main']['temp_max']) - 273.15) * 9 / 5 + 32
+            Temp = (int(data['main']['temp']) - 273.15) * 9 / 5 + 32
+            self.weatherinfo = [data['name'], round(minTemp, 2), round(maxTemp, 2), round(Temp, 2), data['wind']['speed'], data['weather'][0]['description']]
 
-    def returnWeather(self):
+    def getWeatherInfo(self):
         return self.weatherinfo
 
-
-def test():
-    cityname = 'Brooklyn'
-    getWeather=Weatherdata(1, cityname)
-    adapter = Adapter(getWeather)
-    printdata  = Getdata(adapter)
-    printdata.processData()
-
-
-if __name__ == "__main__":
-    test()
