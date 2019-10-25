61658eb3af3e09449fb1051aeb4d986e3ef71ae9
zxl1987 <zxl1987@g.rit.edu>
2019-10-25 15:43:07 -0400

    Create DB.py

---
+++ Project/Data/DB.py
@@ -0,0 +1,11 @@
+import MySQLdb
+
+class DB:
+	try:
+		db = MySQLdb.connect(host='localhost', user='root', passwd='student', db='weather')
+		cur = db.cursor()
+		cur.execute("select * from user;")
+		result = cur.fetchall()
+		print result
+	except:
+	    print("Service Downtime. Please try again later.")
