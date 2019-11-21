cc4427786908a86f12d20aef5886e71353de5142
zxl1987 <zxl1987@g.rit.edu>
2019-11-21 01:20:53 -0500

    updated

--- Project/Model/UserData.py
+++ Project/Model/UserData.py
@@ -7,7 +7,6 @@ userInfo=userInformation()
 
 def verify(username, password):
     verifyUser=getDB.authUser(username, password);
-    print verifyUser
     if verifyUser:
         userInfo.setUserid(verifyUser[0][0])
         return True
@@ -16,17 +15,15 @@ def verify(username, password):
 
 
 def signUp(username, password):
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
     if ' ' in username or ' ' in password:
         return "User name or password can't contain the space"
-    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
-    if (regex.search(username) != None):
+    elif (regex.search(username) != None):
         return "User name can't contain the special charater"
-    if getDB.verifyUser(username):
-        return "username already exists please try another one."
-    if getDB.setUser(username, password):
-        return "Account create successfully"
-    else:
-        return "Fail to create account"
-
+    elif getDB.verifyUser(username):
+        return  "Username already exists please try another one."
+    
+    getDB.setUser(username, password)
+    return True
 if __name__ == '__main__':
     verify("fenglin", "password")
