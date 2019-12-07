c9ba1b37953d40c49e5221859b2fbb1dc788b309
fxl2968 <55754876+Fenglin2968@users.noreply.github.com>
2019-12-07 16:29:52 -0500

    Some Update

--- Project/Model/test.py
+++ Project/Model/test.py
@@ -2,16 +2,13 @@ from UserData import *
 getUser=UserData()
 def test():
 
-    print getUser.verify("fenglin","password")
+    print getUser.verify("fenglin","newP")
     '''
     print getUser.signUp("fegli1", "password","Feng@gmail.com")
     print getUser.setInformation("Feng", "Lin", "1996-08-06", "Windish")
     '''
-    print getUser.viewUserHistory()
-    print getUser.deleteHisiotry("10")
-    print getUser.viewUserHistory()
-    print getUser.saveHistory(1, "wc")
-    print getUser.viewUserHistory()
+    print getUser.changeUserEmail("hello@gmail.com")
+
 
 if __name__ == "__main__":
     test()
